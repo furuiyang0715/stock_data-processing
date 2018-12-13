483309c6bf08d988df45401070a31c7ff8e1eb42
furuiyang <ruiyang0715@gmail.com>
2018-12-13 13:53:06 +0800

    增加对于fix_factor 接口的面向过程测试

--- xx/financeMongo.py
+++ xx/financeMongo.py
@@ -5,7 +5,7 @@ import datetime
 import pandas
 
 from xx.generate_map_factor2table import connect_db, generate_table_field_list, connect_coll
-from xx.stock_convert_tool import convert_11code
+from xx.stock_convert_tool import convert_11code, little11code
 from xx.distribution_factor2table import factor_table
 from xx.time_tool import convert2datetime
 from .factor import f
@@ -44,27 +44,27 @@ class Finance(AbstractJZData):
         固定了因子的表格，表的行索引是股票，列索引是时间
         """
 
-        # [000001.XSHE, 000702.XSHE] --> 不变
+        # ["000001.XSHE", "000702.XSHE"] --> 不变
         stock_list = convert_11code(stock_list)
 
-        # {"comcn_balancesheet": CashEquivalents}
+        # {"comcn_balancesheet": f("CashEquivalents")}
         _factor_table = factor_table(factor, self.factor2table)
 
         # collection --> "comcn_balancesheet"
         # field --> CashEquivalents
-        collection, field = list(factor_table.items())[0]  # 因为是fix_factor所以只有一个因子
+        collection, field = list(_factor_table.items())[0]  # 因为是fix_factor所以只有一个因子
 
-        # # field --> "CashEquivalents"
+        # field --> "CashEquivalents"
         field = field[0].name
 
-        # "2014-08-22" --> datetime.date(2014, 8, 22)
+        # "2014-08-22" --> datetime.datetime(2014, 8, 22)
         start_date = convert2datetime(start_date)
 
         # if not self.full_table[collection]:  # 如果数据是原始季报数据，start_date 改为去年元旦
         #     _year = int(start_date.strftime("%Y"))-1
         #     start_date = datetime.datetime(_year, 1, 1, 0, 0, 0)
 
-        # "2017-09-30" --> datetime.date(2017, 9, 30)
+        # "2017-09-30" --> datetime.datetime(2017, 9, 30)
         end_date = convert2datetime(end_date)
         end_date = end_date + datetime.timedelta(hours=23, minutes=59, seconds=59)
 
@@ -72,12 +72,12 @@ class Finance(AbstractJZData):
         db_coll = connect_coll(collection, self._db)
 
         if not self.full_table[collection]:  # 如果数据是原始季报数据
-            snap = ['code', 'PubDate', field]  # 要返回的字段
+            snap = ['SecuCode', 'PubDate', field]  # 要返回的字段
             doc_snap = {k: 1 for k in snap}
             doc_snap["_id"] = 0
 
             ret = db_coll.find(
-                {'code': {'$in': stock_list},
+                {'SecuCode': {'$in': stock_list},
                  "PubDate": {"$gte": start_date, "$lte": end_date}}, doc_snap)
 
         # else:
@@ -88,16 +88,17 @@ class Finance(AbstractJZData):
         #         {'code': {'$in': stock_list},
         #          "tradingday": {"$gte": start_date, "$lte": end_date}}, doc_snap)
 
-        ret = pandas.DataFrame(list(ret))
+        pandas_ret = pandas.DataFrame(list(ret))
 
-        if not ret.empty:
-            ret[snap[1]] = ret[snap[1]].map(lambda x: x.strftime('%Y-%m-%d'))
-            # ret['code'] = ret['code'].map(lambda x: x[2:] + little11code(x[:2]))
-            ret = ret[snap]
-            ret.columns = ['stock', 'time', field]
-            ret = pandas.crosstab(ret['time'], ret['stock'], values=ret[field], aggfunc='last')
+        if not pandas_ret.empty:
+            pandas_ret[snap[1]] = pandas_ret[snap[1]].map(lambda x: x.strftime('%Y-%m-%d'))
+            # ret['SecuCode'] = pandas_ret['SecuCode'].map(lambda x: x[2:] + little11code(x[:2]))
+            pandas_ret = pandas_ret[snap]
+            pandas_ret.columns = ['stock', 'time', field]
+            pandas_ret = pandas.crosstab(pandas_ret['time'], pandas_ret['stock'],
+                                         values=pandas_ret[field], aggfunc='last')
 
-        return ret
+        return pandas_ret
 
     def fix_symbol(self, stock: str, factors: list or f, start_date: datetime.date or str,
                    end_date: datetime.date or str):
@@ -107,3 +108,65 @@ class Finance(AbstractJZData):
                  trade_date: datetime.date or str):
         pass
 
+
+# --------------------------------------test-------------------------------------------------------
+stock_list = ["000001.XSHE", "000702.XSHE"]
+stock_format = r'^\d{6}\.[A-Z]{4}$'
+# import re
+# re.match(stock_format, stock_list[0])
+# stock_list.sort()
+stock_list = convert_11code(stock_list)  # ['000001.XSHE', '000702.XSHE']
+
+_factor_table = {"comcn_balancesheet": f("CashEquivalents")}
+collection = "comcn_balancesheet"
+field = "CashEquivalents"
+import datetime
+start_date = datetime.datetime(2014, 8, 22)
+end_date = datetime.datetime(2017, 9, 30)
+end_date = end_date + datetime.timedelta(hours=23, minutes=59, seconds=59)
+
+import os
+import pymongo
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://127.0.0.1:27017/")
+mongo = pymongo.MongoClient(MONGO_URI)
+
+db_ = mongo["datacenter"]
+coll = db_["comcn_balancesheet"]
+snap = ['SecuCode', 'PubDate', field]
+doc_snap = {k: 1 for k in snap}
+doc_snap["_id"] = 0
+ret = coll.find({'SecuCode': {'$in': stock_list},
+                 'PubDate': {"$gte": start_date, "$lte": end_date}
+                 }, doc_snap)
+pandas_ret = pandas.DataFrame(list(ret))
+# 更新返回给用户的日期显示格式
+pandas_ret[snap[1]] = pandas_ret[snap[1]].map(lambda x: x.strftime('%Y-%m-%d'))
+pandas_ret = pandas_ret[snap]
+pandas_ret.columns = ['stock', 'time', field]
+pandas_ret = pandas.crosstab(pandas_ret['time'], pandas_ret['stock'],
+                             values=pandas_ret[field], aggfunc='last')
+# stock        000001.XSHE   000702.XSHE
+# time
+# 2014-08-22           NaN  7.220362e+07
+# 2014-10-24  2.773250e+11  8.040438e+07
+# 2015-03-13  2.299240e+11           NaN
+# 2015-03-24           NaN  5.755935e+07
+# 2015-04-24  2.667050e+11           NaN
+# 2015-04-29           NaN  1.129723e+08
+# 2015-08-14  2.986180e+11           NaN
+# 2015-08-21           NaN  9.560868e+07
+# 2015-10-23  2.972230e+11  9.563517e+07
+# 2016-03-10  3.062980e+11           NaN
+# 2016-04-08           NaN  5.240234e+07
+# 2016-04-21  2.961440e+11           NaN
+# 2016-04-28           NaN  1.065683e+08
+# 2016-08-12  2.781780e+11           NaN
+# 2016-08-26           NaN  1.257344e+08
+# 2016-10-21  3.039360e+11           NaN
+# 2016-10-26           NaN  8.364714e+07
+# 2017-03-17  2.917150e+11           NaN
+# 2017-04-14           NaN  5.670373e+07
+# 2017-04-22  2.695410e+11           NaN
+# 2017-04-26           NaN  6.904191e+07
+# 2017-08-11  2.899770e+11           NaN
+# 2017-08-24           NaN  7.251137e+07
