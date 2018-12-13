e5a23aca936dd2f5fdfd752c38177e562bdc2ca1
furuiyang <ruiyang0715@gmail.com>
2018-12-13 16:24:43 +0800

    完成fix_symbol；增加测试

--- xx/financeMongo.py
+++ xx/financeMongo.py
@@ -1,17 +1,22 @@
 """
 finance 的 mongo 版本
 """
+import sys
+import os
+
 import datetime
 import pandas
 import numpy
 
+sys.path.append(os.getcwd())
+
 from xx.generate_map_factor2table import connect_db, generate_table_field_list, connect_coll
-from xx.stock_convert_tool import convert_11code, little11code
-from xx.distribution_factor2table import factor_table
+from xx.stock_convert_tool import convert_11code, little11code, convert_8code
+from xx.distribution_factor2table import factor_table, factor_name_list
 from xx.time_tool import convert2datetime
 from xx.trade_calendar import TradeCalendar
-from .factor import f
-from .interface import AbstractJZData
+from xx.factor import f
+from xx.interface import AbstractJZData
 
 
 class Finance(AbstractJZData):
@@ -100,9 +105,83 @@ class Finance(AbstractJZData):
 
     def fix_symbol(self, stock: str, factors: list or f, start_date: datetime.date or str,
                    end_date: datetime.date or str):
-        pass
+        """
+        :param stock:
+        :param factors:
+        :param start_date:
+        :param end_date:
+        :return:
+        """
+        # 格式化参数
+        table = factor_table(factors, self.factor2table)
+        start_date = convert2datetime(start_date)
+        end_date = convert2datetime(end_date)
+        end_date = end_date + datetime.timedelta(hours=23, minutes=59, seconds=59)
+
+        # merge 停牌时间
+        index = TradeCalendar().calendar(start_date.strftime("%Y%m%d"),
+                                         end_date.strftime("%Y%m%d"))
+        calendar = TradeCalendar().calendar(start_date, end_date)
+        trading_calendar = calendar['date'][calendar['trade']]
+        trading_calendar_index = pandas.DataFrame(trading_calendar,
+                                                  columns=['index']).set_index('index')
+        ret = trading_calendar_index.copy()
+
+        for collection in table:
+            _db = connect_db()
+            db_coll = connect_coll(collection, _db)
+            factors = factor_name_list(table[collection])
+            snap = [None, ]
+            snap.extend(factors)  # [None, 'ClientDeposit', 'TradingAssets']
+            if self.full_collection[collection]:
+                # snap[0] = "tradingday"
+                # doc_snap = {k: 1 for k in snap}
+                # doc_snap["_id"] = 0
+                # data = db_coll.find({'code': stock,
+                #                      "tradingday": {"$gte": start_date, "$lte": end_date}},
+                #                     doc_snap)
+                pass
+
+            else:
+                _year = int(start_date.strftime("%Y")) - 1
+                start_date = datetime.datetime(_year, 1, 1, 0, 0, 0)
+                snap[0] = 'PubDate'
+                doc_snap = {k: 1 for k in snap}
+                doc_snap["_id"] = 0
+                data = db_coll.find({'SecuCode': stock,
+                                     "PubDate": {"$gte": start_date, "$lte": end_date}},
+                                    doc_snap)
+
+            data = pandas.DataFrame(list(data))
+
+            if not data.empty:
+                data[snap[0]] = data[snap[0]].map(lambda x: x.strftime('%Y-%m-%d'))
+                data = data[snap]
+                data = data.set_index(snap[0])
+
+                # 日历填充
+                ret = ret.merge(data, left_index=True, right_index=True, how='outer')
+
+                if not self.full_collection[collection]:
+                    ret[factors] = ret[factors].fillna(method='pad')  # 先向后填充数据
+                    ret = ret.ix[trading_calendar_index.index]  # 再以日历限制一次日期
+
+        ret = ret.astype(float)
+        ret = ret.to_records()
+        ret.dtype.names = ['date'] + list(ret.dtype.names)[1:]
+
+        return numpy.array(ret)
 
     def fix_time(self, stock_list: list or str, factors: list or f,
                  trade_date: datetime.date or str):
         pass
+
+
+if __name__ == "__main__":
+    rundemo = Finance()
+    s1 = datetime.datetime(2016, 1, 1)
+    s2 = datetime.datetime(2017, 1, 1)
+    f_list = [f("SubtotalOperateCashInflow"), f("CashEquivalents"), f("OtherCashInRelatedOperate")]
+    res = rundemo.fix_symbol("000702.XSHE",f_list, s1, s2)
+    print(res)
 
