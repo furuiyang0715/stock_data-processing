6f384e8f8adaa9c9cd77bf0ff6c5e6521a9a005f
furuiyang <ruiyang0715@gmail.com>
2018-12-13 16:33:06 +0800

    删除无用的测试

--- xx/generate_map_factor2table.py
+++ xx/generate_map_factor2table.py
@@ -25,24 +25,3 @@ def generate_table_field_list(coll_name, db_name="datacenter"):
     factor2table = dict(zip(field_name_list, db_name_list))
     return factor2table
 
-
-#-----------------------------------------------test-----------------------------------------------
-# import pymongo
-# import os
-# mongocli = pymongo.MongoClient(os.environ.get("JZDATAURI", "mongodb://127.0.0.1:27017"))
-# client = mongocli
-# db_name = "datacenter"
-# db = client['{}'.format(db_name)]
-# coll_name = "comcn_balancesheet"
-# coll = db['{}'.format(coll_name)]
-#
-# item = coll.find().next()
-# # mongodb数据库中所有字段名组成的列表
-# field_name_list = list(item.keys())
-# # 去除原始mysql数据库的id和mongodb数据库中的_id
-# field_name_list.remove("id")
-# field_name_list.remove("_id")
-# # 生成一个与field_name_list等长的db_name_list
-# db_name_list = [db_name]*len(field_name_list)
-# factor2table = dict(zip(field_name_list, db_name_list))
-
